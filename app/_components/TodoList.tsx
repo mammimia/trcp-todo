a83d2d3b68ecb5e56cad97f629d66efbb3ee8418
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-07-16 13:51:08 +0300

    ✨ (TodoList.tsx): Add input field and button to add todos dynamically 📝 (server/index.ts): Refactor getTodos query to fetch todos from Prisma database 📝 (server/index.ts): Add addTodo mutation to create new todo in Prisma database

--- app/_components/TodoList.tsx
+++ app/_components/TodoList.tsx
@@ -1,9 +1,35 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import { trcp } from '../_trpc/client';
+import { Todos } from '@prisma/client';
 
 export default function TodoList() {
+  const [input, setInput] = useState('');
+
+  const addTodo = trcp.addTodo.useMutation();
   const getTodos = trcp.getTodos.useQuery();
 
-  return <div>{JSON.stringify(getTodos.data)}</div>;
+  return (
+    <div className="flex flex-col gap-4 items-center">
+      <div>{JSON.stringify(getTodos.data)}</div>
+      <input
+        className="text-black"
+        type="text"
+        value={input}
+        onChange={(event) => {
+          setInput(event.target.value);
+        }}
+      />
+      <button
+        className="bg-slate-400 rounded-md w-40 h-10"
+        onClick={async () => {
+          await addTodo.mutateAsync(input);
+          getTodos.refetch();
+          setInput('');
+        }}
+      >
+        Add Todo
+      </button>
+    </div>
+  );
 }
--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,26 @@
+import { PrismaClient, Todos } from '@prisma/client';
 import { publicProcedure, router } from './trcp';
+import { z } from 'zod';
+
+const prisma = new PrismaClient();
 
 export const appRouter = router({
   helloWorld: publicProcedure.query(async () => {
     return 'Hello, World!';
   }),
   getTodos: publicProcedure.query(async () => {
-    return [
-      { id: 1, text: 'Buy milk' },
-      { id: 2, text: 'Buy eggs' },
-      { id: 3, text: 'Buy bread' }
-    ];
+    return await prisma.todos.findMany();
+  }),
+  addTodo: publicProcedure.input(z.string()).mutation(async (opts) => {
+    console.log(opts);
+    if (!opts.input || opts.input === '' || opts.input?.trim() === '') {
+      throw new Error('Input is required');
+    }
+    return await prisma.todos.create({
+      data: {
+        content: opts.input
+      }
+    });
   })
 });
 
